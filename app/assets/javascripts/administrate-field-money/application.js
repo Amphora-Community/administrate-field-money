3b3f4ae7f13ed0d1ce0632e97780e171488599d7
Steven Klavins <stevenk@amphora-research.com>
2021-10-14 12:31:47 +0100

    try load

--- app/assets/javascripts/administrate-field-money/application.js
+++ app/assets/javascripts/administrate-field-money/application.js
@@ -6,7 +6,7 @@ $(function() {
       syncUnmaskedValueWithHiddenField,
       $applyMaskMoneyTo,
       setupMaskMoney,
-      EVENTS_TO_SYNC = 'change keyup paste';
+      EVENTS_TO_SYNC = 'change keyup paste load';
 
   getUnmaskedMoneyCents = function($el) {
     return $el.maskMoney('unmasked')[0];
