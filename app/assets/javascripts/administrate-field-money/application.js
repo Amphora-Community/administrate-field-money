f2e821e08b05a9ed2e6657244535a9f8d3686e9b
Steven Klavins <stevenk@amphora-research.com>
2021-10-14 15:56:11 +0100

    another try

--- app/assets/javascripts/administrate-field-money/application.js
+++ app/assets/javascripts/administrate-field-money/application.js
@@ -36,8 +36,7 @@ $(function() {
 
       console.log(this.value)
       if (this.value === null || this.value === "" || this.value === "$0.00"){
-        console.log("made it")
-      this.value = 0.00
+        syncUnmaskedValueWithHiddenField()
       }
 
   };
