5d1c39c5ad7ed7356d9153e5c30349cc358dc87a
Steven Klavins <stevenk@amphora-research.com>
2021-10-14 13:49:41 +0100

    try calling syncUnmaskedValueWithHiddenField() from start

--- app/assets/javascripts/administrate-field-money/application.js
+++ app/assets/javascripts/administrate-field-money/application.js
@@ -10,31 +10,27 @@ $(function() {
 
   getUnmaskedMoneyCents = function($el) {
 
-    console.log("getUnmaskedMoneyCents")
-    var unmasked = $el.maskMoney("unmasked")[0];
-
     return unmasked ? unmasked : 0;
   };
 
   updateSiblingHiddenFieldValueOf = function($el, getNewValue) {
-    console.log("updateSiblingHiddenFieldValueOf")
     $el.siblings('[type="hidden"]').val(
       getNewValue($el)
     );
   };
 
   syncUnmaskedValueWithHiddenField = function() {
-    console.log("syncUnmaskedValueWithHiddenField")
     updateSiblingHiddenFieldValueOf($(this), getUnmaskedMoneyCents);
   };
 
   $applyMaskMoneyTo = function(el) {
-    console.log("applyMaskMoneyTo")
     return $(el).maskMoney().trigger('mask');
   };
 
   setupMaskMoney = function() {
-    console.log("setupMaskMoney ")
+
+    syncUnmaskedValueWithHiddenField()
+
     $applyMaskMoneyTo(this)
       .on(
         EVENTS_TO_SYNC,
@@ -43,5 +39,4 @@ $(function() {
   };
 
   $('[data-maskmoney]').each(setupMaskMoney);
-  console.log("main function <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 });
