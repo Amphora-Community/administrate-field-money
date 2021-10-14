ccf2106cd5703bb6212ef76d490b6ae74ba3beb5
Steven Klavins <stevenk@amphora-research.com>
2021-10-14 15:39:41 +0100

    test

--- app/assets/javascripts/administrate-field-money/application.js
+++ app/assets/javascripts/administrate-field-money/application.js
@@ -37,7 +37,7 @@ $(function() {
       console.log(this.value)
       if (this.value === null || this.value === "" || this.value === "$0.00"){
         console.log("made it")
-      this.value("0.00")
+      this.value ="0.00"
       }
 
   };
